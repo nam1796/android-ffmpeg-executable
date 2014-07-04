7d4ee92890a7d77d59538e054732f44dec711d77
Shoichi Takagi <fly1tkg@gmail.com>
2014-07-04 15:01:59 +0900

    forgot export

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86
-SYSROOT=$NDK/platforms/android-9/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86
+export SYSROOT=$NDK/platforms/android-9/arch-arm/
 
 ./build_x264.sh
 ./build_ffmpeg.sh
